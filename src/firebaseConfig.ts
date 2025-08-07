80074df727c13b33fdf04886e1dfc10d99d261f1
Rohael Naeem <rohael@MacBook-Air-2.local>
2025-08-07 19:45:12 +0400

    Refactor: move secrets to env file, remove from code

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,19 +3,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// --- YOUR FIREBASE CONFIG ---
-// (You can move these to .env.local later if you want)
 const firebaseConfig = {
-  apiKey: "AIzaSyAWszwwj7r3aQfYFnrGDcHElctKIpEszeY",
-  authDomain: "theclearpath-6864e.firebaseapp.com",
-  projectId: "theclearpath-6864e",
-  storageBucket: "theclearpath-6864e.appspot.com",
-  messagingSenderId: "424848862076",
-  appId: "1:424848862076:web:b7d709da0fed000122d2f9",
-  measurementId: "G-RNFQXLZ6DD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Avoid re-initializing app on hot reloads
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 export const auth = getAuth(app);
