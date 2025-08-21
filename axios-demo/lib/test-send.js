006678de1136780f3586dabcd226702d02cea48f
rohaelnj <rohael1995@gmail.com>
2025-08-21 21:13:14 +0400

    test: add unit test for sendWelcomeEmail

--- axios-demo/lib/test-send.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({ path: '../../.env' }); // <-- add this at the very top
-
-const sendWelcomeEmail = require('./sendBrandedEmail');
-
-sendWelcomeEmail('rohaelnaeem1995@gmail.com', 'Rohael');
